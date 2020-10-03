ba9f96b774738eff2e0a9cb265e7f072b916f55e
bue221 <camiloplaza3@gmail.com>
2020-10-02 22:05:45 -0500

    Updates

---
+++ precache-manifest.084f613d1f58ae287aaf2d3539896f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ca97643e8e01aa8ce3201ed476b034",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "55779a816418c72ddb3c",
+    "url": "/netflix-clone/static/css/main.95aafdfb.chunk.css"
+  },
+  {
+    "revision": "ff84254010ff35c052d0",
+    "url": "/netflix-clone/static/js/2.7afaf92b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netflix-clone/static/js/2.7afaf92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55779a816418c72ddb3c",
+    "url": "/netflix-clone/static/js/main.e2e76e3b.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
